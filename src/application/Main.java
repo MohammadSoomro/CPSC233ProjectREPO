64a2426cf800d1824c1e75f7394ed21552f98bc7
moiz.sidddiqui <moiz.sidddiqui@IC-CS233-14.uc.ucalgary.ca>
2022-08-01 00:54:28 -0600

    moiz did this

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			System.out.println("hellow worlds");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
